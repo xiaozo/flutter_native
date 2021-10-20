f391d09ce7018bae53e7d1e6fcece45c402d5d97
zoulixiang <807581848@qq.com>
2021-10-20 18:17:05 +0800

    Update sp_size.dart

--- lib/ui/sp_size.dart
+++ lib/ui/sp_size.dart
@@ -2,5 +2,5 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 extension Sp on num {
   double get sph => ScreenUtil().scaleWidth * this;
-  double get ascale => this.w;
+  double get as => this.w;
 }
